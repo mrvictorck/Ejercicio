5ff6067945ac04de1a71665945bf7bcd127bf8ed
Victor Codina Solano <145485328+mrvictorck@users.noreply.github.com>
2024-12-20 08:09:30 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+  return {
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Node', 'Express', 'MongoDB'],
+    getSkills: function() {
+      return this.skills;
+    }
+  };
+}
